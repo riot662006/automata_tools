5f66afeee6c904ee2aedb03efc0072b7cd2c7e0b
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-09-13 15:11:28 -0500

    simulate.py: Add input file argument

--- cli/simulate.py
+++ cli/simulate.py
@@ -6,22 +6,36 @@ from automata.utils import cprint
 
 
 def main():
-    ap  = argparse.ArgumentParser(description="Simulate DFA on input strings from a .dfauto file")
+    ap = argparse.ArgumentParser(
+        description="Simulate DFA on input strings from a .dfauto file")
     ap.add_argument("input", help="Path to .dfauto file")
-    ap.add_argument("strings", nargs="+", help="Input strings to simulate on the DFA")
-    ap.add_argument("--out", "-o", help="Optional path to save results into a file")
+    ap.add_argument("strings", nargs="*",
+                    help="Input strings to simulate on the DFA")
+    ap.add_argument("--in", dest="infile",
+                    help="File containing input strings (space and/or newline separated)")
+    ap.add_argument(
+        "--out", "-o", help="Optional path to save results into a file")
     args = ap.parse_args()
 
     dfa = parse_dfa_file(args.input)
+
+    words = list(args.strings)
+    if args.infile:
+        file_words = Path(args.infile).read_text(encoding="utf-8").split()
+        words.extend([word for word in file_words if word.strip()])
+
+    if not words:
+        ap.error(
+            "No input strings provided (either as arguments or via --strings-file).")
+
     output_lines = []
-    
-    for word in args.strings:
+
+    for word in words:
         msg = f"{word!r} -> "
 
         try:
             accepted = dfa.accepts(word)
-            
-            
+
             if accepted:
                 msg += "Accepted"
                 cprint(msg, color="green")
