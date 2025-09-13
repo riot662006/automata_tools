524e2305c0744c6ec1e90d3aad250aeacd0d661b
Onyekachi Ibekwe <ibekwe2006@gmail.com>
2025-09-13 13:54:29 -0500

    simulate.py: Simulate and print result in console

--- cli/simulate.py
+++ cli/simulate.py
@@ -1,12 +1,22 @@
 import argparse
 
+from automata.parser import parse_dfa_file
+
 def main():
     ap  = argparse.ArgumentParser(description="Simulate DFA on input strings from a .dfauto file")
     ap.add_argument("input", help="Path to .dfauto file")
     ap.add_argument("strings", nargs="+", help="Input strings to simulate on the DFA")
     args = ap.parse_args()
 
-    print(f"Simulating DFA from {args.input} on input strings: {args.strings}")
+    dfa = parse_dfa_file(args.input)
+    
+    for word in args.strings:
+        try:
+            is_accepted = dfa.accepts(word)
+            
+            print(f"Input: {word!r} -> {'Accepted' if is_accepted else 'Rejected'}")
+        except ValueError as e:
+            print(f"Input: {word!r} -> Error: {e}")
 
 if __name__ == "__main__":
     main()
